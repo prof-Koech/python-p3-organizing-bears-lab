ca113fdafd2a4dfc2ae6d1052b8fb05b4caa111c
Kennedy <kiplangatkennedy019@gmail.com>
2023-05-30 08:47:22 +0300

    partially done

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO bears (name, sex)
+VALUES(
+        "Mr. Chocolate" 'M',
+        "Rowdy" 'F',
+        "Tabitha" 'F',
+        "Sergeant Brown" 'M',
+        "Melisa" 'F',
+        "Grinch" 'M',
+        "Wendy" 'M',
+        " "
+    );
+-- "M"
+-- "F"
